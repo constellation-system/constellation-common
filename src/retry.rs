126dcff44c47e3b166edf112b59ab3c94b93714b
Eric McCorkle <eric@metricspace.net>
2025-05-31 14:34:15 -0400

    Add terrestrial network resubmission default (#34)

--- src/retry.rs
+++ src/retry.rs
@@ -162,6 +162,18 @@ impl Default for Retry {
 }
 
 impl Retry {
+    /// Default `Retry` parameters for resubmitting large object
+    /// transfers on normal terrestrial networks.
+    pub const TERRESTRIAL_LARGE_OBJ_RESUB_DEFAULT: Retry = Retry {
+        factor: 300000000,
+        exp_base: 2.0,
+        exp_factor: 1.0,
+        exp_rounds_cap: 20,
+        linear_factor: 0.0,
+        linear_rounds_cap: None,
+        addend: 0,
+        max_random: 100000
+    };
     /// Default `Retry` parameters for normal terrestrial networks.
     pub const TERRESTRIAL_NETWORK_DEFAULT: Retry = Retry {
         factor: 100000,
